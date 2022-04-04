03d61344114e321ee8e15c4ac8f15019f97a54ab
prochor666 <prochor666@gmail.com>
2022-04-04 16:04:41 +0200

    Meta widget defaults

--- src/vue-components/SiteMetaTypes.vue
+++ src/vue-components/SiteMetaTypes.vue
@@ -7,7 +7,7 @@
             v-for="lang in langs" :key="lang.id"
         >
             <div class="py-2">
-                <label class="inline-block mb-4" :for="`name-${lang.alpha2}`">
+                <label class="inline-block mb-4 text-sm font-medium" :for="`name-${lang.alpha2}`">
                     Name ({{ lang.name }})
                 </label>
 
@@ -38,7 +38,99 @@
             <div class="mt-1">
 
                 <div class="block" v-if="['text'].includes(meta.type)">
-                    <span class="text-md">Text options</span>
+
+                    <div
+                        class="
+                            sm:grid sm:grid-cols-2
+                            sm:gap-4
+                            sm:items-start
+                            sm:border-t sm:border-gray-200 sm:dark:border-gray-900
+                            sm:pt-5
+                        "
+                    >
+                        <label
+                            :for="`default-${lang.alpha2}`"
+                            class="
+                                block
+                                text-sm
+                                font-medium
+                                text-gray-700
+                                dark:text-gray-400
+                                sm:mt-px
+                                sm:pt-2
+                            "
+                        >
+                            Default ({{ lang.name }})
+                        </label>
+                        <div class="mt-1 sm:mt-0">
+                            <input
+                                :id="`default-${lang.alpha2}`"
+                                name="name"
+                                type="text"
+                                autocomplete="off"
+                                :placeholder="`Enter default ${lang.name.toLowerCase()} value`"
+                                class="
+                                    block
+                                    max-w-lg
+                                    w-full
+                                    shadow-sm
+                                    focus:ring-sky-500
+                                    focus:border-sky-500
+                                    sm:text-sm
+                                    border-gray-300
+                                    rounded-md
+                                    dark:text-gray-400
+                                    dark:bg-gray-900
+                                    dark:border-gray-700
+                                "
+                                v-model="meta.options[lang.alpha2].defaults.value"
+                            />
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="block" v-if="['blob'].includes(meta.type)">
+
+                    <div
+                        class="
+                            sm:grid sm:grid-cols-3
+                            sm:gap-4
+                            sm:items-start
+                            sm:border-t sm:border-gray-200  sm:dark:border-gray-900
+                            sm:pt-5
+                        "
+                    >
+                        <label
+                            :for="`default-${lang.alpha2}`"
+                            class="
+                                block
+                                text-sm
+                                font-medium
+                                text-gray-700
+                                dark:text-gray-400
+                                sm:mt-px
+                                sm:pt-2
+                            "
+                        >
+                            Default ({{ lang.name }})
+                        </label>
+                        <div class="mt-1 sm:mt-0 sm:col-span-2">
+
+                            <textarea
+                                type="text"
+                                name="aliases"
+                                :id="`default-${lang.alpha2}`"
+                                rows="6"
+                                :placeholder="`Enter default ${lang.name.toLowerCase()} value`"
+                                class="border-gray-300 dark:border-gray-700 focus:ring-sky-500 focus:border-sky-500 flex-1 block w-full max-w-lg min-w-0 rounded-md sm:text-sm dark:text-gray-400 dark:bg-gray-900"
+                                v-model="meta.options[lang.alpha2].defaults.value"
+                            ></textarea>
+
+                        </div>
+                    </div>
+
+
                 </div>
 
                 <div class="block" v-if="['radio', 'select'].includes(meta.type)">
@@ -50,6 +142,10 @@
                 </div>
 
                 <div class="block" v-if="['bool'].includes(meta.type)">
+                    <span class="text-md">Bool options</span>
+                </div>
+
+                <div class="block" v-if="['files'].includes(meta.type)">
                     <span class="text-md">Bool options</span>
                 </div>
 
